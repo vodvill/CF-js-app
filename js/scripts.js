cf42cf5e4797775f33a5c386708b762ab2daa6e4
vodvill <65613101+vodvill@users.noreply.github.com>
2020-06-17 09:05:59 -0500

    created pokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
-alert('Hello World');
-var favoriteFood = 'pizza';
-document.write(favoriteFood);
+var pokemonList = [
+{
+  creatureName : 'Carnivine',
+  creatureHeight : 4.07,
+  creatureAbilities : ['Levitate'],
+  creatureTypes : ['Grass'],
+},
+{
+  creatureName : 'Metapod',
+  creatureHeight : 2.04,
+  creatureAbilities : ['Shed-skin'],
+  creatureTypes : ['Bug'],
+},
+{
+  creatureName : 'Charmander',
+  creatureHeight : 2.00,
+  creatureAbilities : ['Blaze', 'Solar-power'],
+  creatureTypes : ['Fire'],
+},
+{
+  creatureName : 'Squirtle',
+  creatureHeight : 0.5,
+  creatureAbilities : ['Rain-dish', 'Torrent'],
+  creatureTypes : ['Monster', 'Water  1'],
+}
+]
+console.log(pokemonList)
